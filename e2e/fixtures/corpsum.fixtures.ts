f4ce1f13a8f0e280a5118335bf3c2de05e47d749
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-02-01 18:27:09 +0100

    login fixture with promise

--- e2e/fixtures/corpsum.fixtures.ts
+++ e2e/fixtures/corpsum.fixtures.ts
@@ -9,10 +9,12 @@ export const test = base.extend<NonNullable<unknown>, { account: Account }>({
 	account: [{ username: "", password: "" }, { scope: 'worker' }],
 	page: async ({ page, account }, use) => {
 		const { username, password } = account;
-		await page.goto('/login');
+		await page.goto('/en/login');
 		await page.getByLabel('Username').fill(username);
 		await page.getByLabel('Password').fill(password);
+		const loginPromise = page.waitForResponse('https://noskecrystal5corpsum.acdh-dev.oeaw.ac.at/run.cgi/corpora');
 		await page.locator('#main-content').getByRole('button', { name: 'Login' }).click();
+		await loginPromise;
 		await use(page);
 	},
 });
