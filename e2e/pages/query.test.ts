5e73b286e011b762e8e0de38099e97766dbf2dbe
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-02-02 17:04:29 +0100

    query interface test extended

--- e2e/pages/query.test.ts
+++ e2e/pages/query.test.ts
@@ -1,24 +1,49 @@
 import { expect } from '@playwright/test';
 
-import { test } from '../fixtures/corpsum.fixtures';
+import { login } from '../fixtures/corpsum.fixtures';
 
-const account = { username: "corpsum", password: "zM9wS1lR9uJ6zR9s" };
-test.use({ account });
+const account = { username: process.env.TEST_USER!, password: process.env.TEST_PASSWORD! };
+login.use({ account });
 
-test.describe("Query Interface", () => {
-	test('should be able to perform and display multiple queries', async ({ page }) => {
-
-		await page.getByRole('button', { name: 'Corpus Open' }).click();
-		await page.getByText('amc_4.2').click();
-		await page.getByPlaceholder('Your search term').fill('haus');
+login.describe("Query Interface", () => {
+	login('should be able to display information on multiple queries', async ({ page }) => {
+		await page.getByRole('combobox').first().click();
+		const corpusPromise = page.waitForResponse('https://noskecrystal5corpsum.acdh-dev.oeaw.ac.at/run.cgi/corp_info?corpname=amc_4.2&subcorpora=1&format=json');
+		await page.getByRole('option', { name: 'amc_4.2' }).click();
+		await corpusPromise;
+		await expect(page.locator('form')).toContainText('Number of tokens:');
+		await page.getByPlaceholder('Your search term').fill('haus')
 		await page.getByPlaceholder('Your search term').press('Enter');
+		await expect(page.locator('div:nth-child(1) > .v-card-title > div > .text-xl')).toBeVisible();
+		await expect(page.locator('div:nth-child(1) > .v-card-title > div > .text-xl')).toHaveText('haus');
+		await expect(page.locator('.flex > span > .v-chip__content').first()).toBeVisible()
 		await expect(page.locator('.flex > span > .v-chip__content').first()).toHaveText('amc_4.2');
+		await expect(page.locator('.flex > span > .v-chip__content').nth(1)).toBeVisible();
+		await expect(page.locator('.flex > span > .v-chip__content').nth(1)).toHaveText('iqueryrow');
 
-		await page.getByRole('button', { name: 'amc_4.2 Open' }).click();
-		await page.getByText('amc_3.2').click();
+		await page.getByRole('combobox').first().click();
+		await page.getByRole('option', { name: 'amc_3.2' }).click();
+		await page.getByRole('combobox').nth(2).click();
+		await page.getByRole('option', { name: 'Word search' }).click();
 		await page.getByPlaceholder('Your search term').click();
 		await page.getByPlaceholder('Your search term').fill('tomate');
 		await page.getByPlaceholder('Your search term').press('Enter');
+		await expect(page.locator('div:nth-child(2) > .v-card-title > div > .text-xl')).toBeVisible();
 		await expect(page.locator('div:nth-child(2) > .v-card-title > div > .text-xl')).toHaveText('tomate');
+		await expect(page.locator('div:nth-child(2) > .v-card-text > .flex > span > .v-chip__content').first()).toBeVisible()
+		await expect(page.locator('div:nth-child(2) > .v-card-text > .flex > span > .v-chip__content').first()).toHaveText('amc_3.2');
+		await expect(page.locator('div:nth-child(2) > .v-card-text > .flex > span > .v-chip__content').nth(1)).toBeVisible();
+		await expect(page.locator('div:nth-child(2) > .v-card-text > .flex > span > .v-chip__content').nth(1)).toHaveText('wordrow');
+	});
+	login('should be able select a corpus and select the available subcorpora', async ({ page }) => {
+		await page.getByRole('combobox').first().click();
+		const corpusPromise = page.waitForResponse('https://noskecrystal5corpsum.acdh-dev.oeaw.ac.at/run.cgi/corp_info?corpname=amc_4.2&subcorpora=1&format=json');
+		await page.getByRole('option', { name: 'amc_4.2' }).click();
+		await corpusPromise;
+		await expect(page.locator('form')).toContainText('Number of tokens:');
+		await page.getByRole('combobox').nth(1).click();
+		await page.locator('div').filter({ hasText: /^AustriazismenSFB$/ }).first().click();
+		await expect(page.locator('form')).toContainText('tokens:');
+
 	});
 });
