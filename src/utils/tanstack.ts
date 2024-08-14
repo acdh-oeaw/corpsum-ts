6393b754ee6b6829ab328e019f4a4ad8ee6e81f2
Lukas Moertl <moertllukas@gmail.com>
2024-08-14 15:41:45 +0200

    duplicate import

--- src/utils/tanstack.ts
+++ src/utils/tanstack.ts
@@ -3,9 +3,6 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import type { Ref } from "vue";
 
-export function cn(...inputs: Array<ClassValue>) {
-	return twMerge(clsx(inputs));
-}
 
 // eslint-disable-next-line
 export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
