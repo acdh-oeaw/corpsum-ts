23721f34904cd5c72391c69698683e39ca2bcbed
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-09-16 19:18:27 +0200

    feat: localeSwitcher for login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 import { cn } from '@/utils/styles'
-const t = useTranslations("LoginPage");
 definePageMeta({
 	title: "LoginPage.meta.title",
 	layout: "full-page",
 });
+
+const t = useTranslations("LoginPage");
 
 const localeRoute = useLocaleRoute();
 const { locale } = useI18n();
@@ -29,40 +30,36 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 h-screen">
-		<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+	<MainContent class="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 h-screen">
+		<div class="relative h-full flex-col p-10 text-white dark:border-r lg:flex">
 			<div class="absolute inset-0 bg-zinc-900" />
 			<div class="relative text-7xl font-bold">
 				CorpSum
 			</div>
 			<div class="relative mt-auto">
-				<blockquote class="space-y-2">
 					<p class="text-lg">
-						&ldquo;This library has saved me countless hours of work and
-						helped me deliver stunning designs to my clients faster than
-						ever before.&rdquo;
+						{{ t("splashtext") }}
 					</p>
-					<footer class="text-sm">
-						Sofia Davis
-					</footer>
-				</blockquote>
 			</div>
 		</div>
-		<div class="lg:p-8">
-			<div :class="cn('grid gap-6', $attrs.class ?? '')">
-				<form>
+		<div class="h-full lg:p-4">
+			<div class="flex justify-end">
+				<LocaleSwitcher />
+			</div>
+			<div :class="cn('grid gap-6 max-w-96 h-full mx-auto', $attrs.class ?? '')">
+				<form class="my-auto">
 					<div class="grid gap-2">
 						<div class="grid gap-1">
 							<Input
 								id="username"
-								placeholder="username"
+								:placeholder="t('username')"
 								type="text"
 								v-model="username"
 								:disabled="isLoading"
 							/>
 							<Input
 								id="password"
-								placeholder="password"
+								:placeholder="t('password')"
 								type="password"
 								v-model="password"
 								:disabled="isLoading"
@@ -75,5 +72,5 @@ onMounted(async () => {
 				</form>
 			</div>
 		</div>
-	</div>
+	</MainContent>
 </template>
