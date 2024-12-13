807bdf4c495532f2032101d0bcc3b2914dcb4c4d
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2024-12-13 16:15:43 +0100

    feat: open-API fetch client

---
+++ src/composables/use-noske-client.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import-x/no-duplicates */
+import type { Middleware } from "openapi-fetch";
+import createClient from "openapi-fetch";
+
+import type { paths } from "@/types/noske.schema";
+
+const myMiddleware: Middleware = {
+	async onRequest({ request }) {
+		const authStore = useAuth();
+		const { expiry } = storeToRefs(authStore);
+		// refresh token if it expires in less than 2 minutes
+		if (expiry.value - Date.now() < 120000) {
+			await authStore.refresh();
+		}
+		return request;
+	},
+	onError({ error }) {
+		return new Error("Oops, fetch failed", { cause: error });
+	},
+};
+
+export function useNoskeClient(noskeInstance: string) {
+	const client = createClient<paths>({ baseUrl:`/api/noske/${noskeInstance}` });
+	client.use(myMiddleware);
+	return client;
+}
