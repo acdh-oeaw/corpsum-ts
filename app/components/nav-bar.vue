5070984b9d78836262b6eeb8e06bf7edd8726281
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-01-28 17:56:06 +0100

    feat: menu from config

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -3,39 +3,60 @@ const NavItems = [
 	{
 		icon: "Book",
 		tooltip: "Corpora",
+		href: "/corpora",
 	},
 	{
 		icon: "Terminal",
 		tooltip: "Queries",
+		href: "/queries",
 	},
 	{
 		icon: "ChartLine",
 		tooltip: "Visualizations",
+		href: "/visualizations",
 	},
 ];
 </script>
 
 <template>
 	<aside class="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
-		<div class="border-b p-2">
-			<Button aria-label="Home" size="icon" variant="outline"> CS </Button>
-		</div>
+		<NuxtLinkLocale :href="{ path: '/' }">
+			<div class="border-b p-2">
+				<Button aria-label="Home" size="icon" variant="outline"> CS </Button>
+			</div>
+		</NuxtLinkLocale>
 		<nav class="grid gap-1 p-2">
 			<TooltipProvider v-for="(item, index) in NavItems" :key="index">
 				<Tooltip>
 					<TooltipTrigger as-child>
-						<Button
-							:aria-label="item.tooltip"
-							class="rounded-lg bg-muted"
-							size="icon"
-							variant="ghost"
-						>
-							<LucideIcon :name="item.icon" stroke-width="2" />
-						</Button>
+						<NuxtLinkLocale :href="{ path: item.href }">
+							<Button
+								:aria-label="item.tooltip"
+								class="rounded-lg bg-muted"
+								size="icon"
+								variant="ghost"
+							>
+								<LucideIcon :name="item.icon" stroke-width="2" />
+							</Button>
+						</NuxtLinkLocale>
 					</TooltipTrigger>
 					<TooltipContent side="right" :side-offset="5">
 						{{ item.tooltip }}
 					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</nav>
+		<nav class="mt-auto grid gap-1 p-2">
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger as-child>
+						<NuxtLinkLocale :href="{ path: '/imprint' }">
+							<Button aria-label="Imprint" class="rounded-lg bg-muted" size="icon" variant="ghost">
+								<LucideIcon name="Scale" stroke-width="2" />
+							</Button>
+						</NuxtLinkLocale>
+					</TooltipTrigger>
+					<TooltipContent side="right" :side-offset="5"> Imprint </TooltipContent>
 				</Tooltip>
 			</TooltipProvider>
 		</nav>
