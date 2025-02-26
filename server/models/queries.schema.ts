70a0cf8264ab95b37743bd41466e2334e7b21056
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-02-26 11:49:12 +0100

    feat: db schemas

---
+++ server/models/queries.schema.ts
@@ -0,0 +1,28 @@
+import { type InferRawDocType, Schema } from "mongoose";
+
+// eslint-disable-next-line import-x/no-unresolved
+import { defineMongooseModel } from "#nuxt/mongoose";
+
+const schemaDefinition = {
+	_id: { type: Schema.Types.ObjectId },
+	name: { type: "string", required: true },
+	owner: [{ type: Schema.Types.ObjectId, ref: "users", required: true }],
+	noske: { type: Schema.Types.ObjectId, ref: "noskeinstances", required: true },
+	corpus: { type: "string", required: true },
+	subCorpus: { type: "string", required: true },
+	type: {
+		type: "string",
+		enum: ["charrow", "cqlrow", "iqueryrow", "lemmarow", "phraserow", "wordrow"],
+		required: true,
+	},
+	userInput: { type: "string", required: true },
+	facettingValues: { type: Schema.Types.Mixed, required: true },
+} as const;
+
+export type QueryDocument = InferRawDocType<typeof schemaDefinition>;
+
+export const QueryModel = defineMongooseModel({
+	name: "queries",
+	schema: schemaDefinition,
+	options: { timestamps: true },
+});
