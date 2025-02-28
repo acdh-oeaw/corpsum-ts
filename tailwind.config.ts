3094410f91abc00eebeec06c1ed7387d2284e8d5
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-02-28 11:07:08 +0100

    chore: config updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,23 @@ module.exports = {
 					DEFAULT: "hsl(var(--card))",
 					foreground: "hsl(var(--card-foreground))",
 				},
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
+				},
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
+				},
 			},
 			borderRadius: {
 				xl: "calc(var(--radius) + 4px)",
@@ -66,20 +83,36 @@ module.exports = {
 			},
 			keyframes: {
 				"accordion-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-accordion-content-height)" },
+					from: {
+						height: "0",
+					},
+					to: {
+						height: "var(--reka-accordion-content-height)",
+					},
 				},
 				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
+					from: {
+						height: "var(--reka-accordion-content-height)",
+					},
+					to: {
+						height: "0",
+					},
 				},
 				"collapsible-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-collapsible-content-height)" },
+					from: {
+						height: 0,
+					},
+					to: {
+						height: "var(--radix-collapsible-content-height)",
+					},
 				},
 				"collapsible-up": {
-					from: { height: "var(--radix-collapsible-content-height)" },
-					to: { height: 0 },
+					from: {
+						height: "var(--radix-collapsible-content-height)",
+					},
+					to: {
+						height: 0,
+					},
 				},
 			},
 			animation: {
@@ -90,5 +123,6 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [typographyPlugin],
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	plugins: [typographyPlugin, require("tailwindcss-animate")],
 };
