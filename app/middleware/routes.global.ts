788e352fab8e01e3773ae19de22ec61351268930
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2026-05-08 08:11:44 +0200

    fix: i18n/route types

--- app/middleware/routes.global.ts
+++ app/middleware/routes.global.ts
@@ -1,10 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-	const nuxt = useNuxtApp();
 	const auth = useAuth();
-	const localeRoute = useLocaleRoute();
-	const i18n = nuxt.$i18n;
-	const loginPath = localeRoute("/login", i18n.locale.value);
-	if (!auth.isLoggedIn() && to.path.toString() !== loginPath?.path.toString()) {
-		await navigateTo(localeRoute("/login", i18n.locale.value));
+	const localePath = useLocalePath();
+	const { locale } = useI18n();
+	const loginPath = localePath("/login", locale.value);
+	if (!auth.isLoggedIn() && to.path !== loginPath) {
+		await navigateTo(loginPath);
 	}
 });
