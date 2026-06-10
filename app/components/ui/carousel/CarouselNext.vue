bf5b2b72faa80ee2e941da4c04a2d5a0a64a6ad8
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2026-06-10 13:15:25 +0200

    fix: reposition carousel nav

--- app/components/ui/carousel/CarouselNext.vue
+++ app/components/ui/carousel/CarouselNext.vue
@@ -18,8 +18,8 @@ const { orientation, canScrollNext, scrollNext } = useCarousel();
 			cn(
 				'absolute size-8 touch-manipulation rounded-full p-0',
 				orientation === 'horizontal'
-					? '-right-12 top-1/2 -translate-y-1/2'
-					: '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
+					? 'right-2 top-1/2 z-10 -translate-y-1/2'
+					: 'bottom-2 left-1/2 z-10 -translate-x-1/2 rotate-90',
 				props.class,
 			)
 		"
